7e9f7b8413486fc2643d587f3f17781fe479f415
Усманов Марат Равшанович <chelioschew@yandex.ru>
2021-07-18 17:39:54 +0300

    empty project

---
+++ src/main/java/com/fategen/NumerologyBot/NumerologyBotApplication.java
@@ -0,0 +1,13 @@
+package com.fategen.NumerologyBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumerologyBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumerologyBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fategen/NumerologyBot/NumerologyBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fategen.NumerologyBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumerologyBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
